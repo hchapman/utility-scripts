bb3da5ec2d57172ab082b47d27d4af1fe34ac66c
Harrison Chapman <hchaps@gmail.com>
2013-02-04 15:21:20 -0500

    Update eject-rpi-hdd-ascript.sh

--- eject-rpi-hdd-ascript.sh
+++ eject-rpi-hdd-ascript.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 ## SSHs to the Raspberry Pi and then displays an AppleScript dialog if it is successful.
 
-if [ ssh rpi "/etc/init.d/mpd stop && eject /dev/hd1 && /etc/init.d/mpd start" ];
+if (ssh rpi "/etc/init.d/mpd stop && eject /dev/hd1 && /etc/init.d/mpd start")
 then 
-  osascript << EOF
+  osascript <<-EOF
     tell application "System Events"
       activate
-      display dialog "The disk was ejected successfully and may be removed."
+      display alert "The disk was ejected successfully and may be removed."
     end tell
-  EOF
+EOF
 else
-  osascript << EOF
+  osascript <<-EOF
     tell application "System Events"
       activate
-      display dialog "There was a problem ejecting the disk. You should manually try the commands."
+      display alert "There was a problem ejecting the disk. You should manually try the commands."
     end tell
-  EOF
+EOF
 fi
